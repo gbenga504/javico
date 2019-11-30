6277a8ec9af24122b5995e636dacc0de8d45436a
Olusola <olusola@elta.solutions>
2019-11-30 22:06:57 +0100

    Implement menu bar ui

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
+import { Tooltip } from '@material-ui/core';
 
 import './index.css';
 import Icon from '../../atoms/Icon';
 
 const iconList = [
-  { icon: 'search' },
-  { icon: 'expand' },
-  { icon: 'share' },
-  { icon: 'logo-github' },
-  { icon: 'color-palette' },
+  { text: 'Search file', icon: 'ios-search' },
+  { text: 'Make full screen', icon: 'ios-expand' },
+  { text: 'Share code', icon: 'ios-share-alt' },
+  { text: 'Sign in', icon: 'logo-github' },
+  { text: 'Light theme', icon: 'ios-bulb' },
 ];
+
+{
+  /* <ion-icon name="contract"></ion-icon> */
+}
 
 const MenuBar: React.FC = () => {
   return (
     <section className="menubar flex-column">
-      <div className="menubar__title flex-column mt-16 mb-8">
-        <p className="menubar__title--text">P</p>
-      </div>
-      {iconList.map(el => (
-        <div key={el.icon} className="menubar__icon mt-8">
-          <Icon name={el.icon} />
+      <Tooltip title={'placement.js'} placement="bottom" enterDelay={100}>
+        <div className="menubar__title flex-column center mt-16 mb-8">
+          <div className="menubar__title-text flex-column center">
+            <p>P</p>
+          </div>
         </div>
+      </Tooltip>
+      {iconList.map(el => (
+        <Tooltip title={el.text} placement="bottom" enterDelay={100}>
+          <div key={el.icon} className="menubar__icon mt-16">
+            <Icon name={el.icon} />
+          </div>
+        </Tooltip>
       ))}
     </section>
   );
