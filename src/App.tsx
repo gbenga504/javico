610aae3f28ff6380ac100707e35d3aec70e88d10
Olusola <olusola@elta.solutions>
2019-12-05 21:13:46 +0100

    Implement toggle screen size

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import Home from './views/Home';
 
-const App: React.FC = () => <Home />;
+const App: React.FC = () => (
+  <div id="app">
+    <Home />
+  </div>
+);
 
 export default App;
--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tooltip } from '@material-ui/core';
 
 import './index.css';
 import { Typography, Icon } from '../../atoms';
 
-const iconList = [
-  { text: 'Search file', icon: 'ios-search' },
-  { text: 'Make full screen', icon: 'ios-expand' },
-  { text: 'Share code', icon: 'ios-share-alt' },
-  { text: 'Sign in', icon: 'logo-github' },
-  { text: 'Light theme', icon: 'ios-bulb' },
+var doc = window.document;
+var docEl = doc.documentElement;
+
+var requestFullScreen = docEl.requestFullscreen; // || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+var cancelFullScreen = doc.exitFullscreen; //|| doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+var notFullScreen = () => !doc.fullscreenElement; // && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement;
+
+const iconList = (screenSize: boolean) => [
+  { text: 'Search file', action: '', icon: 'ios-search' },
+  {
+    text: screenSize ? 'Make full screen' : 'Resize screen',
+    action: 'toggleFullScreen',
+    icon: screenSize ? 'ios-expand' : 'ios-contract',
+  },
+  { text: 'Share code', action: '', icon: 'ios-share-alt' },
+  { text: 'Sign in', action: '', icon: 'logo-github' },
+  { text: 'Light theme', action: '', icon: 'ios-bulb' },
 ];
 
 const MenuBar: React.FC = () => {
+  const [fullScreen, toggleFullScreen] = useState(notFullScreen());
+
+  useEffect(() => {
+    window.addEventListener('resize', function() {
+      if (window.screen.width === window.innerWidth) {
+        toggleFullScreen(notFullScreen());
+      }
+    });
+  }, []);
+
+  function handleToggleFullScreen() {
+    if (fullScreen) {
+      requestFullScreen.call(docEl).then(() => toggleFullScreen(!fullScreen));
+    } else {
+      if (cancelFullScreen) {
+        cancelFullScreen.call(doc).then(() => toggleFullScreen(!fullScreen));
+      }
+    }
+  }
+
+  function triggerAction(action: string) {
+    switch (action) {
+      case 'toggleFullScreen':
+        handleToggleFullScreen();
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <section className="menubar flex-column">
       <Tooltip title={'placement.js'} placement="bottom" enterDelay={100}>
@@ -22,15 +63,17 @@ const MenuBar: React.FC = () => {
           </div>
         </div>
       </Tooltip>
-      {iconList.map(el => (
-        <div key={el.icon} className="menubar__icon">
-          <Tooltip title={el.text} placement="bottom" enterDelay={100}>
-            <span style={{ display: 'flex' }}>
-              <Icon name={el.icon} />
-            </span>
-          </Tooltip>
-        </div>
-      ))}
+      {iconList(fullScreen).map(el => {
+        return (
+          <div key={el.icon} className="menubar__icon" onClick={() => triggerAction(el.action)}>
+            <Tooltip title={el.text} placement="bottom" enterDelay={100}>
+              <span style={{ display: 'flex' }}>
+                <Icon name={el.icon} />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      })}
     </section>
   );
 };
