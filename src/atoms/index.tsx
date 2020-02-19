dc830fe9f06e42b068b288cbb4dbe3a7979a148d
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2020-02-19 09:36:29 +0100

    Delete Icon file in Atom

--- src/atoms/index.tsx
+++ src/atoms/index.tsx
@@ -1,5 +1,4 @@
 export { default as Typography } from './Typography';
-export { default as Icon } from './Icon';
 export { default as AnimatedCircularLoader } from './AnimatedCircularLoader';
 export { default as IndeterminateLinearProgress } from './IndeterminateLinearProgress';
 export { NotificationProvider, withNotificationBanner } from './NotificationBanner';
