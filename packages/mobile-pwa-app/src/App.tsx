f3ed9634489d6516e8dd930ef2e31ddbe97fc557
Olusola <olusola@elta.solutions>
2020-03-02 00:45:22 +0100

    Layout mobile ui

--- packages/mobile-pwa-app/src/App.tsx
+++ packages/mobile-pwa-app/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core';
 
 import { theme } from './Css';
+import Editor from './views/Editor';
 
 const App: React.FC = () => {
   return (
     <MuiThemeProvider theme={theme}>
-      <span>Welcome to the home page</span>
+      <Editor />
     </MuiThemeProvider>
   );
 };
