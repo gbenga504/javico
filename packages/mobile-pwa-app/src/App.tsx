73e6131f8d0cab2ddb56fd05ea26cc776efe1a6e
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2020-03-01 15:50:12 +0100

    Add initial packages for pwa mobile

--- packages/mobile-pwa-app/src/App.tsx
+++ packages/mobile-pwa-app/src/App.tsx
@@ -1,26 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core';
 
-function App() {
+import { theme } from './Css';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <span>Welcome to the home page</span>
+    </MuiThemeProvider>
   );
-}
+};
 
 export default App;
