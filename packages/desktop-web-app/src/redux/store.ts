a5b2dec01504dfde965b9d4eba5f45d10e82f831
Olusola <olusola@elta.solutions>
2020-03-06 22:40:44 +0100

    Add redux devtool

--- packages/desktop-web-app/src/redux/store.ts
+++ packages/desktop-web-app/src/redux/store.ts
@@ -1,5 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import { logger } from 'redux-logger';
 
-export default createStore(reducers, applyMiddleware(logger));
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(logger));
+
+export default createStore(reducers, enhancer);
