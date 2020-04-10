01eae422c7c9f01b8381ef524092f2d8f09176d3
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2020-04-10 13:28:40 +0100

    Add index.ts file to the components root

---
+++ packages/components/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as DeleteMessageModal } from "./DeleteMessageModal";
+export { default as EditMessagePanel } from "./EditMessagePanel";
+export { default as InlineCodeComment } from "./InlineCodeComment";
+export { default as MarkDownRenderer } from "./MarkDownRenderer";
+export { default as MonacoEditor } from "./MonacoEditor";
+export { default as SignInViaGithubModal } from "./SignInViaGithubModal";
+export { default as Terminal } from "./Terminal";
+export { default as SyntaxHighlighter } from "./SyntaxHighlighter";
