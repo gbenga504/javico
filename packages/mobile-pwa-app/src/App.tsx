0bacfdfd025f957a36e5fb59174f456fc9008611
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2020-04-28 23:03:44 +0100

    Rework route

--- packages/mobile-pwa-app/src/App.tsx
+++ packages/mobile-pwa-app/src/App.tsx
@@ -1,48 +1,28 @@
-import React, { useState } from 'react';
-import { MuiThemeProvider } from '@material-ui/core';
+import React from 'react';
+import { MuiThemeProvider, makeStyles } from '@material-ui/core';
 import { theme } from '@javico/common/lib/design-language/Css';
 
-import TabNavigator from './components/TabNavigator';
-import ReadMe from './views/ReadMe';
-import ActionsModal from './components/ActionsModal';
-import Editor from './views/Editor';
-// import Settings from './views/Settings';
-// import MenuDrawer from './components/MenuDrawer';
-
-export type IMenus = 'editor' | 'comment' | 'readme' | 'action';
+import Routes from './Routes';
 
 const App: React.FC = () => {
-  const [activeMenu, setActiveMenu] = useState<IMenus>('editor');
-  const [isActionsModalVisible, setIsActionsModalVisible] = useState<boolean>(false);
-
-  function handleToggleActionsModal() {
-    setIsActionsModalVisible(prevState => !prevState);
-  }
+  const classes = useStyles();
 
   return (
     <MuiThemeProvider theme={theme}>
-      {/* <MenuDrawer onBlur={() => setIsSideBarVisible(false)} isSideBarVisible={isSideBarVisible} /> */}
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '100%',
-        }}>
-        <div style={{ flex: 1 }}>
-          {activeMenu === 'readme' && <ReadMe />}
-          {activeMenu === 'editor' && <Editor />}
-        </div>
-        <TabNavigator
-          activeMenu={activeMenu}
-          onSetActiveMenu={setActiveMenu}
-          onHandleToggleActionsModal={handleToggleActionsModal}
-        />
+      <div className={classes.container}>
+        <Routes />
       </div>
-      {/* <Settings /> */}
-      <ActionsModal onRequestClose={handleToggleActionsModal} isVisible={isActionsModalVisible} />
     </MuiThemeProvider>
   );
 };
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+  },
+});
 
 export default React.memo(App);
